79595008892fc01f1f7fd662f916538a632a18b2
Cbster <cbster@protonmail.com>
2021-05-16 20:10:43 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from binascii import crc32 as _crc32
+from words import words
+
+prtl_seed = input("Enter known seed words: ").split()
+missing_po = int(input("At which position is the word missing? (First word being 1)\n"))-1
+
+
+def mn_checksum(wlist):
+    """Given a mnemonic seed word list, return a string of the seed checksum."""
+    if len(wlist) > 13:
+        wlist = wlist[:24]
+    else:
+        wlist = wlist[:12]
+    wstr = "".join(word[:3] for word in wlist)
+    z = ((_crc32(wstr.encode()) & 0xffffffff) ^ 0xffffffff) >> 0
+    z2 = ((z ^ 0xffffffff) >> 0) % len(wlist)
+    return wlist[z2]
+
+
+def mn_validate_checksum(wlist):
+    """Given a mnemonic seed word list, check if checksum word is valid.
+    Returns boolean value.
+    """
+    return True if mn_checksum(wlist) == wlist[-1] else False
+
+
+possibles = []  # List of possible missing words
+for word in words:
+    test_seed = prtl_seed[0:missing_po] + [word] + prtl_seed[missing_po:]  # Seed to have checksum tested
+    if mn_validate_checksum(test_seed):
+        possibles += [word]
+        continue
+    else:
+        continue
+print(f"There are {len(possibles)} possibilities for the missing word. These are:\n")
+for word in possibles:
+    print(word)
